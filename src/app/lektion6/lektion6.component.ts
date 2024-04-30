4857c7adf4cad62055084ed64e9b32fa4429c302
Mattias Fucik <107649652+AdamWallgren28@users.noreply.github.com>
2024-04-30 13:24:21 +0200

    sets up lektion6 and sets up a broken test

--- src/app/lektion6/lektion6.component.ts
+++ src/app/lektion6/lektion6.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 
+// Uppgift 6.1
+function addNumbers(a: number, b: number): number {
+  let sum = a+b;
+  return sum;
+}
+console.log(addNumbers(1, 2))
+
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(addNumbers(1, 2)).toBe(3);
+});
+
 @Component({
   selector: 'app-lektion6',
   standalone: true,
@@ -7,6 +19,9 @@ import { Component } from '@angular/core';
   templateUrl: './lektion6.component.html',
   styleUrl: './lektion6.component.css'
 })
-export class Lektion6Component {
 
+
+export class Lektion6Component {
+  public svarSixOne : string = `Svar 6.1:  Funktion att testa: 1 + 2 = ${addNumbers(1, 2)}`;
+  public testSixOne : string = ``;
 }
